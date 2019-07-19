7b34cc1f07f62587ea6b3d5dfe1300dd9a901683
gemmagentles <multimediadesign.gemmagentles@gmail.com>
2019-07-19 13:19:34 -0700

    Use the wordpress loop and ajax in wp-admin to make the load more button work correctly. Add a div wrapper around the specific card and keep the button inside the section but outside of the card wrapper. Then inject the new posts by using js append so it goes inside the div wrapper but at the bottom and just before the button. Wrap the button around logic so it doesnt show if there are no posts.

--- wp-content/themes/rentestate/archive-listing.php
+++ wp-content/themes/rentestate/archive-listing.php
@@ -2,41 +2,35 @@
 
 	<main role="main">
 		<!-- section -->
-		<section class="article-cards__grid">
+		<section>
+			<div class="article-cards__grid">
+				<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
-			<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+					<?php get_template_part('template-parts/blocks/article-cards'); ?> 		
 
-				<?php get_template_part('template-parts/blocks/article-cards'); ?> 		
+				<?php endwhile; ?>
 
-			<?php endwhile; ?>
+				<?php else: ?>
 
-			<?php else: ?>
+				<!-- article -->
+				<article>
 
-			<!-- article -->
-			<article>
+					<h1><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h1>
 
-				<h1><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h1>
+				</article>
+				<!-- /article -->
 
-			</article>
-			<!-- /article -->
-
-			<?php endif; ?>
-			<?php get_template_part('pagination'); ?>
-
+				<?php endif; ?>
+			</div>
+			<?php
+			global $wp_query; // you can remove this line if everything works for you
+			
+			// don't display the button if there are not enough posts
+			if (  $wp_query->max_num_pages > 1 )
+				echo '<div class="article-cards__load-button-wrapper"><button id="js-load-more-btn" class="article-cards__load-button">Load More</button></div>';
+			?>
 		</section>
 		<!-- /section -->
-
-		<?php
-		global $wp_query; // you can remove this line if everything works for you
-		
-		// don't display the button if there are not enough posts
-		if (  $wp_query->max_num_pages > 1 )
-			echo '<section class="article-cards__load-button-wrapper"><button id="js-load-more-btn" class="article-cards__load-button">Load More</button></section>';
-		?>
-
-		<!-- <section class="article-cards__load-button-wrapper"> -->
-			<!-- <button id="js-load-more-btn" class="article-cards__load-button">Load More</button> -->
-		<!-- </section> -->
 
 		<section>
 			<?php get_template_part('template-parts/blocks/cta-banner-listings'); ?> 		
--- wp-content/themes/rentestate/js/article-cards.js
+++ wp-content/themes/rentestate/js/article-cards.js
@@ -17,14 +17,12 @@ jQuery(function($){ // use jQuery code inside this to avoid "$ is not defined" e
 			},
 			success : function( data ){
 				if( data ) { 
-					button.text( 'More posts' ).prev().before(data); // insert new posts
-                    article_loadmore_params.current_page++;
-                    
+                    button.text( 'More....' );
+                    $( ".article-cards__grid" ).append(data); // insert new posts
+					article_loadmore_params.current_page++;
+ 
 					if ( article_loadmore_params.current_page == article_loadmore_params.max_page ) 
 						button.remove(); // if last page, remove the button
- 
-					// you can also fire the "post-load" event here if you use a plugin that requires it
-					// $( document.body ).trigger( 'post-load' );
 				} else {
 					button.remove(); // if no data, remove the button as well
 				}
