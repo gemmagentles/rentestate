20627dc25f8330d2f02a730a92879ff95ce2a3ff
gemmagentles <multimediadesign.gemmagentles@gmail.com>
2019-07-29 11:53:58 -0700

    test change

--- wp-content/themes/rentestate/rentestate-template.php
+++ wp-content/themes/rentestate/rentestate-template.php
@@ -1,4 +1,4 @@
-<?php /* Template Name: Rent Estate Page Template */ get_header(); ?>
+<?php /* Template Name: Rent Estate: Page Template */ get_header(); ?>
 
 	<main role="main">
 		<!-- section -->
